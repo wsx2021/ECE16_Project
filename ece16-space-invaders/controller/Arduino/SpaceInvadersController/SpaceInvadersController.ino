c0a2f7904694ab97600a8f321b7db3184b70c43b
wsx2021 <80902225+wsx2021@users.noreply.github.com>
2021-11-17 09:06:28 -0800

    Add files via upload

---
+++ ece16-space-invaders/controller/Arduino/SpaceInvadersController/SpaceInvadersController.ino
@@ -0,0 +1,57 @@
+/*
+ * Global variables
+ */
+// Acceleration values recorded from the readAccelSensor() function
+int ax = 0; int ay = 0; int az = 0;
+int ppg = 0;        // PPG from readPhotoSensor() (in Photodetector tab)
+int sampleTime = 0; // Time of last sample (in Sampling tab)
+bool sending;
+int i = 0;
+/*
+ * Initialize the various components of the wearable
+ */
+void setup() {
+  setupAccelSensor();
+  setupCommunication();
+  setupDisplay();
+  setupPhotoSensor();
+  sending = false;
+
+  writeDisplay("Ready...", 1, true);
+  writeDisplay("Set...", 2, false);
+  writeDisplay("Play!", 3, false);
+}
+
+/*
+ * The main processing loop
+ */
+void loop() {
+  readAccelSensor();
+  // Parse command coming from Python (either "stop" or "start")
+  String command = receiveMessage();
+  if(command == "stop") {
+    sending = false;
+    writeDisplay("Controller: Off", 0, true);
+  }
+  else if(command == "start") {
+    sending = true;
+    writeDisplay("Controller: On", 0, true);
+  }
+  else if(command == "2"){
+    activateMotor(255);
+    writeDisplay("Fire", 0, true);
+ delay(1000);
+     deactivateMotor();
+   
+    
+    }
+  else if (command != ""){
+    writeDisplay(const_cast<char*>(command.c_str()), i, true);
+    }
+
+  // Send the orientation of the board
+  if(sending && sampleSensors()) {
+    sendMessage(String(getOrientation()));
+  }
+  
+}
